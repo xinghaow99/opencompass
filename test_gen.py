5383ad27379c255a800e09d445ed01f2feb26fdf
Xinghao <xinghaow99@gmail.com>
2024-02-26 20:12:36 +0800

    update phi-2

--- test_gen.py
+++ test_gen.py
@@ -3,7 +3,8 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 # model_name_or_path = "google/gemma-2b"
-model_name_or_path = "internlm/internlm2-1_8b"
+# model_name_or_path = "internlm/internlm2-1_8b"
+model_name_or_path = "microsoft/phi-2"
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="auto", torch_dtype=torch.float16, trust_remote_code=True)
 
