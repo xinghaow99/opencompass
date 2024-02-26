abf60d13e380adf28550c9fde76c39e0b4fe5a53
Xinghao <xinghaow99@gmail.com>
2024-02-26 19:56:05 +0800

    update gemma-2b

---
+++ run_evaluations.sh
@@ -0,0 +1,3 @@
+python run.py \
+    --datasets ceval_ppl mmlu_ppl \
+    --models hf_internlm2_1_8b 
---
+++ test_gen.py
@@ -0,0 +1,14 @@
+# pip install accelerate
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# model_name_or_path = "google/gemma-2b"
+model_name_or_path = "internlm/internlm2-1_8b"
+tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+model = AutoModelForCausalLM.from_pretrained(model_name_or_path, device_map="auto", torch_dtype=torch.float16, trust_remote_code=True)
+
+input_text = "My name is Julien and I like to"
+input_ids = tokenizer(input_text, return_tensors="pt").to("cuda")
+
+outputs = model.generate(**input_ids, max_length=100)
+print(tokenizer.decode(outputs[0]))
